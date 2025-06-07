db649f45c60a35089e265bd99ce285d4b7607981
Kwadwo Owusu Ansah <owusuansahkwadwo24@gmail.com>
2025-06-07 19:12:34 +0000

    removed docker and compose

--- pollproject/settings.py
+++ pollproject/settings.py
@@ -140,12 +140,12 @@ SIMPLE_JWT = {
 if DEBUG:
     DATABASES = {
         'default': {
-            'ENGINE': config('DB_ENGINE', default='django.db.backends.postgresql'),
-            'NAME': config('DB_NAME', default='votingsystem'),
-            'USER': config('DB_USER', default='quarjo'),
-            'PASSWORD': config('DB_PASSWORD', default='quarjowusu'),
-            'HOST': config('DB_HOST', default='db'),
-            'PORT': config('DB_PORT', default='5432'),
+            'ENGINE': config('DB_ENGINE'),
+            'NAME': config('DB_NAME'),
+            'USER': config('DB_USER'),
+            'PASSWORD': config('DB_PASSWORD'),
+            'HOST': config('DB_HOST'),
+            'PORT': config('DB_PORT'),
         }
     }
 else:
